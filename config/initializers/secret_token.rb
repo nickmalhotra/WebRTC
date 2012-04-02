ed54f342bfcb02b1958275f9286a3b065918c823
Nikhil Malhotra <nickmalhotra@yahoo.com>
2012-04-02 19:22:17 -0400

    Made changes to public index.html to include web rtc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebrtcTest::Application.config.secret_token = 'ed76550e27a4ce63d47c4b4e165839c2154bf1317c219d084dbd47ba6228bfc0184e7661a385e55e56e19af4b07601374b8924933dbf18163bcaae8e331b294e'
